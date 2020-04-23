2c52572a1ef2b82cf9260e7097696e3e1d69e864
MohsenAlajmi <63060630+MohsenAlajmi@users.noreply.github.com>
2020-04-24 01:48:29 +0300

    Login & reset password

---
+++ 86h1/login.php
@@ -0,0 +1,159 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: welcome.php");
+    exit;
+}
+
+// Include config file
+require_once "config.php";
+
+// Define variables and initialize with empty values
+ $password = $email ="";
+$password_err = $email_err = "";
+
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    // Check if username is empty
+    if(empty(trim($_POST["email"]))){
+        $email_err = "Please enter username.";
+    } else{
+        $email = trim($_POST["email"]);
+    }
+
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+
+    // Validate credentials
+    if(empty($email_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT id, email, password FROM users WHERE email = ?";
+
+        if($stmt = mysqli_prepare($connect, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_email);
+
+            // Set parameters
+            $param_email = $email;
+
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id, $email, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["email"] = $email;
+
+                            // Redirect user to welcome page
+                            header("location: welcome.php");
+                        } else{
+                            // Display an error message if password is not valid
+                            $password_err = "The password you entered was not valid.";
+                        }
+                    }
+                } else{
+                    // Display an error message if username doesn't exist
+                    $email_err = "No account found with that email.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+
+    // Close connection
+    mysqli_close($connect);
+}
+?>
+
+<!DOCTYPE html>
+<html style="background-color: rgb(46,15,123);">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>86h login</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body style="background-color: rgb(46,15,123);">
+    <nav class="navbar navbar-light navbar-expand-md" style="background-color: rgb(46,15,123);">
+        <div class="container-fluid"><img src="assets/img/76dc75b0-7f18-4306-9bc8-32e1641adfc1.jpg" width="70px" height="70px" alt="logo"><a class="navbar-brand" href="#" style="color: rgb(230,255,255);">&nbsp; &nbsp;86H</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div
+                class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item" role="presentation"></li>
+                    <li class="nav-item" role="presentation"></li>
+                    <li class="nav-item" role="presentation"></li>
+                </ul>
+        </div>
+        </div>
+    </nav>
+    <div class="login-clean" style="background-color: rgb(15,7,67);">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" style=" background-color: rgb(52,33,128);">
+            <h2 class="sr-only">Login Form</h2>
+            <div class="illustration"><i class="icon ion-ios-navigate" style="color: rgb(137,71,244);"></i></div>
+
+            <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+            <input class="form-control" type="email" name="email" placeholder="Email" value="<?php echo $email; ?>">
+            <span class="help-block"><?php echo $email_err; ?></span>
+            </div>
+
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+            <input class="form-control" type="password" name="password" placeholder="Password">
+            <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+
+            <div class="form-group"><a class="btn btn-primary btn-block" role="button" href="Group_page.html" style="background-color: rgb(137,71,244);">Log In</a>
+              <a class="btn btn-primary btn-block" role="button" href="Create_Account.php" style="background-color: rgb(137,71,244);">Create account</a>
+            </div>
+            <a class="forgot" href="ResetPassword.php" style="color: rgb(255,255,255);">Forgot your email or password?</a>
+        </form>
+    </div>
+    <div class="footer-basic" style="background-color: rgb(46,15,123);">
+        <footer style="background-color: rgb(46,15,123);padding: 8px;">
+            <div class="social"><a href="#" style="color: rgb(230,255,255);"><i class="icon ion-social-instagram" style="color: rgb(230,255,255);"></i></a><a href="#" style="color: rgb(230,255,255);"><i class="icon ion-social-snapchat" style="color: rgb(230,255,255);"></i></a>
+                <a
+                    href="#"><i class="icon ion-social-twitter" style="color: rgb(230,255,255);"></i></a><a href="#" style="color: rgb(230,255,255);"><i class="icon ion-social-facebook" style="color: rgb(230,255,255);"></i></a></div>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="#" style="color: rgb(230,255,255);">Home</a></li>
+                <li class="list-inline-item"><a href="#" style="color: rgb(230,255,255);">Services</a></li>
+                <li class="list-inline-item"><a href="#" style="color: rgb(230,255,255);">About</a></li>
+                <li class="list-inline-item"><a href="#" style="color: rgb(230,255,255);">Terms</a></li>
+                <li class="list-inline-item"><a href="#" style="color: rgb(230,255,255);">Privacy Policy</a></li>
+            </ul>
+            <p class="copyright" style="color: rgb(230,255,255);">Company Name © 2017</p>
+        </footer>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
