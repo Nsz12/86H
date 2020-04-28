e2760c651031a457019a2389f8245b4d748e2e73
Nasser Aldossary <Nsz12@users.noreply.github.com>
2020-04-29 00:12:57 +0300

    Update welcome.php

--- 86h1/welcome.php
+++ 86h1/welcome.php
@@ -1,6 +1,11 @@
 <?php
 // Initialize the session
 session_start();
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: index.php");
+    exit;
+}
 // Include config file
 require_once "config.php";
 //get information about the user for the database
